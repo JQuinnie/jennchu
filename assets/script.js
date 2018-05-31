f3a0e63c8ee93690a9d5901e8dbe6331627aad2b
JQuinnie <jenn8866@gmail.com>
2018-05-30 23:01:02 -0600

    Add jquery to function

--- assets/script.js
+++ assets/script.js
@@ -29,8 +29,9 @@ window.addEventListener('keyup', (e) => {
   pressed.push(e.key);
   pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
   if (pressed.join('').includes(secretCode)) {
-    console.log('ding ding');
-    cornify_add();
+    $.getScript('http://www.cornify.com/js/cornify.js', function () {
+      cornify_add();
+    })
   }
   console.log(pressed);
 })
